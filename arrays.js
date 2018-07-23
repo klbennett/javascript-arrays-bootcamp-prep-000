2e4d522349858ba366b800713da7951e89808425
Kay B <kaylouisebennett@gmail.com>
2018-07-23 23:27:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,11 +36,12 @@ return array
 
 function removeElementFromEndOfArray(array) {
   array.slice(0, array.length - 1)
-return myArraySlice  
+return array
 }
   
 
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
+  return array
 }
 
